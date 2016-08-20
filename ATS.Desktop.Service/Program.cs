defe3fe1149f444f8c45166c64519e0abcc86d99
Alexander Karagulamos <developer@netrifik.com>
2016-08-20 07:51:52 +0100

    Testing Teamcity Notification
    Signed-off-by: Alexander Karagulamos <developer@netrifik.com>

--- ATS.Desktop.Service/Program.cs
+++ ATS.Desktop.Service/Program.cs
@@ -6,7 +6,7 @@ namespace ATS.Desktop.Service
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application. 
         /// </summary>
         static void Main()
         {
