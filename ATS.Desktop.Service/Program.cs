0c88f098ca7b9eb4f5d02d5b12c8944acfbbbfd0
Alexander Karagulamos <developer@netrifik.com>
2016-08-21 04:01:07 +0100

    Testing TeamCity Tray Notification
    Signed-off-by: Alexander Karagulamos <developer@netrifik.com>

--- ATS.Desktop.Service/Program.cs
+++ ATS.Desktop.Service/Program.cs
@@ -5,7 +5,7 @@ namespace ATS.Desktop.Service
 {
     static class Program
     {
-        /// <summary> 
+        /// <summary>
         /// The main entry point for the application. 
         /// </summary>
         static void Main()
